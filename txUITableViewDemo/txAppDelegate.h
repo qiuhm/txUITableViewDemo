9b489060489b88edbece2f343a5b345c6c27731a
萱的软件测试博客 <qiuhmm@gmail.com>
2013-12-02 19:00:17 +0800

    Initial Commit

---
+++ txUITableViewDemo/txAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  txAppDelegate.h
+//  txUITableViewDemo
+//
+//  Created by tingxuan.qhm on 13-12-2.
+//  Copyright (c) 2013年 tingxuan.qhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface txAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
